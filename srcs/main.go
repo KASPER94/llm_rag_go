d12496c04ae2f0cc1f1bec6c34c763b5cda520a7
Simon Kaperski <skapersk@bess-f4r2s2.clusters.42paris.fr>
2025-02-21 12:51:40 +0100

    still going

--- srcs/main.go
+++ srcs/main.go
@@ -13,4 +13,37 @@ var mistralClient *openai.Client
 
 func init() {
 	apiKey := os.Getenv("MISTRAL_API_KEY")
+	if apiKey == "" {
+		log.Fatal("API_KEY missing")
+	}
+}
+
+func generateResponse(c *gin.Contect) {
+	var request struct {
+		Prompt string `json:"prompt`
+	}
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.ShouldBindJSON(http.StatusBadRequest, gin.H{"error": "Wrong Json format"})
+		return
+	}
+	resp, err := mistralClient.CreateChatCompletion(c, openai.ChatCompletionRequest{
+		Model: "mistral-small-latest",
+		Messages: []openai.ChatCompletionMessage{
+			{Role: "user", Content: request.Prompt},
+		},
+	})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Erreur API Mistral: %v", err)})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"response": resp.Choices[0].Message.Content})
+}
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/chat", generateResponse)
+	log.Println("server ok")
 }
